b993c6bc6776e2c74e2f3d8da575a48aa125b262
Jibey-meuh <jbchaux@gmail.com>
2026-06-19 11:26:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if st.button("Déclencher le Conseil"):
                     api_key=api_key
                 )
                 
-                modele_gratuit = "liquid/lfm-40b:free"
+                modele_gratuit = "microsoft/phi-3-medium-128k-instruct:free"
                 
                 # ÉTAPE 1 : IVRE
                 prompt_creative = f"Tu es le conseiller 'Ivre'. Propose 3 idées ultra-créatives, audacieuses et sans aucun filtre pour résoudre : '{problematique}'."
